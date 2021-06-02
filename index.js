1e45ced459fa6a036554c324a2c14a01ae1377b2
ryan <ball.ryan.rb@gmail.com>
2021-06-02 11:17:42 -0600

    adds length.js package

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app = new Vue({
   el: "#app",
   data: function() {
     return {
-      message: "Hello from Vue!"
+      message: "Hello from Vue!",
+      conversion70: length(70, "m").to("ft"),
+      conversionMi: length(1, "mi").to("m"),
     };
   },
   created: {
@@ -17,3 +19,6 @@ var app = new Vue({
 console.log("Hello from Javascript!");
 var startOfDay = moment().startOf('day').fromNow();
 console.log(startOfDay);
+
+var conversion = length(70, "m").to("ft");
+console.log(conversion);
